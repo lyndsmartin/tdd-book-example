56d90d65ed0cbf86ebf78c2ee827b5c70f6a5956
Lindsey Martin <lyndsmartin@gmail.com>
2016-10-20 16:03:32 -0500

    Updated settings file

--- superlists/settings.py
+++ superlists/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
